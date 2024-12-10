100c1c19251e86b030ce7b076a8f3377f9132339
huyhuyqazwsx <huyhuyqaz121233@gmail.com>
2024-12-10 22:38:50 +0700

    cap nhat voi luong

--- main.cpp
+++ main.cpp
@@ -1,15 +1,28 @@
-#include <iostream>
+#include <bits/stdc++.h>
 #include <minizip/unzip.h>
 #include <zlib.h>
 #include <atomic>
-#include <fstream>
 #include <chrono>
 using namespace std;
 
 atomic<bool> check(false);
+string passwordtext;
 
-void trypass(string &zipfile, string &password) {
-    unzFile file=unzOpen(zipfile.c_str())    ;
+string indextranfer(string passwordtext, long long i){
+    long long size= passwordtext.size();
+    string mid="";
+    while(i!=0){
+        long long index= i%size;
+        mid+=passwordtext[index];
+        i=i/size;
+    }
+    reverse(mid.begin(), mid.end());
+    return mid;
+}
+
+void trypass(string &zipfile, long long index, int const numthread, long long const maxindex ) {
+
+    unzFile file=unzOpen(zipfile.c_str());
 
     if (file == NULL) {
         cout<<"Khong mo duoc file zip"<<endl;
@@ -21,57 +34,65 @@ void trypass(string &zipfile, string &password) {
         return;
     }
 
-    if (unzOpenCurrentFilePassword(file, password.c_str())==UNZ_OK) {
-        char dem[1024];
-        int bytetoread= unzReadCurrentFile(file, dem, sizeof(dem));
-        long long crc=0;
-        while (bytetoread > 0) {
-            crc= crc32(crc, (unsigned char *)dem, bytetoread);
-            bytetoread= unzReadCurrentFile(file, dem, sizeof(dem));
-        }
+    //kiem tra mat khau
+    while (!check.load() && index<=maxindex ) {
+        string password= indextranfer(passwordtext, index);
+        index+=numthread;
 
-        unz_file_info file_info;
-        unzGetCurrentFileInfo(file, &file_info,NULL,0,NULL,0,NULL,0);
-        if (file_info.crc==crc) {
-            cout<<"Mat khau tim duoc: "<<password<<endl;
-            check.store(true);
+        if (unzOpenCurrentFilePassword(file, password.c_str())==UNZ_OK) {
+            // cout<<password<<endl;
+            char dem[1024];
+            int bytetoread= unzReadCurrentFile(file, dem, sizeof(dem));
+            long long crc=0;
+
+            while (bytetoread > 0) {
+                crc= crc32(crc, (unsigned char *)dem, bytetoread);
+                bytetoread= unzReadCurrentFile(file, dem, sizeof(dem));
+            }
+
+            unz_file_info file_info;
+            unzGetCurrentFileInfo(file, &file_info,NULL,0,NULL,0,NULL,0);
+            if (file_info.crc==crc) {
+                cout<<"Mat khau tim duoc: "<<password<<endl;
+                check.store(true);
+                unzCloseCurrentFile(file);
+                unzClose(file);
+                return;
+            }
             unzCloseCurrentFile(file);
-            unzClose(file);
-            return;
         }
     }
-
     unzClose(file);
-
 }
 
-void getpassword(string &zipfile,string &passwordfile) {
-    ifstream file(passwordfile.c_str());
-    string password;
-
-    if (file.is_open()!=true) {
-        cout<<"Khong mo duoc file txt"<<endl;
-        return;
-    }
-
-    while (getline(file,password)) {
-        if (check.load()) {
-            break;
-        }
-        // cout<<password<<endl;
-        trypass(zipfile,password);
-    }
-
-}
 
 int main() {
     string zipfile = "D:/testzip/huytestZZZZ.zip"; // duong dan
-    string passwordfile = "D:/testzip/bungnotohop.txt";// dung dan file mat khau
+    passwordtext = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
+    int numpassword=4;
 
+    //nhap so luong
+    int numthread;
+    cout<<"Nhap so luong muon chay:";
+    cin>>numthread;
+
+    int passwordlength= passwordtext.length();
+    long long maxindex= pow(passwordlength,numpassword);
+
+    cout<<"Bat dau chuong trinh:"<<endl;
     auto start = chrono:: high_resolution_clock::now();
 
-    getpassword(zipfile,passwordfile);
+    //chuogn trinh chinh
+    vector<thread> threads;
+
+    for (int i=0;i<numthread;i++) {
+        threads.emplace_back(trypass,ref(zipfile),i,numthread,maxindex);
+    }
+
+    for (auto &th : threads) {
+        th.join();
+    }
 
     auto end = chrono::high_resolution_clock::now();
 
